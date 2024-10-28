03af16e8c62fdfce29ec9ce5bfe63f0ae50e551a
Rida Razak <ridarazak24@gmail.com>
2024-10-28 09:04:42 +0400

    Updated code structure

---
+++ afterschool-club/products.js
@@ -0,0 +1,102 @@
+let products = [
+    {
+        "id": 1001,
+        "subject": "Mathematics",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 150,
+        "image":"images/download.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 1002,
+        "subject": "English",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 130,
+        "image":"images/english.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 1003,
+        "subject": "Physics",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 150,
+        "image":"images/Physics.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 1004,
+        "subject": "Chemistry",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 140,
+        "image":"images/chemistry.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 1005,
+        "subject": "Biology",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 120,
+        "image":"images/B.png",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 1006,
+        "subject": "Art",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 90,
+        "image":"images/art.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 1007,
+        "subject": "Music",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 100,
+        "image":"images/music.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 1008,
+        "subject": "Dance",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 90,
+        "image":"images/dance.png",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 1009,
+        "subject": "Coding",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 130,
+        "image":"images/coding.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 1010,
+        "subject": "Sports",
+        "description": "fun math class",
+        "Location": " London",
+        "price": 100,
+        "image":"images/sports.png",
+        "availableInventory": 5,
+        "rating": 2
+    }
+]
