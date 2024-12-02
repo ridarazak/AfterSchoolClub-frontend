d08b3fcdd2351355d661c2216e6ea57921044c52
Rida Razak <ridarazak24@gmail.com>
2024-12-02 17:17:29 +0400

    added space available to sort

--- afterschool-club/products.js
+++ afterschool-club/products.js
@@ -2,8 +2,7 @@ let products = [
     {
         "id": 1001,
         "subject": "Chess",
-        "description": "fun math class",
-        "Location": " London",
+        "Location": "Academic City",
         "price": 150,
         "image":"images/chess.png",
         "availableInventory": 10,
@@ -12,8 +11,7 @@ let products = [
     {
         "id": 1002,
         "subject": "Piano",
-        "description": "Upgrade your english skills",
-        "Location": " Austrailia",
+        "Location": "Barsha",
         "price": 130,
         "image":"images/piano.png",
         "availableInventory": 10,
@@ -22,8 +20,7 @@ let products = [
     {
         "id": 1003,
         "subject": "Karate",
-        "description": "fun math class",
-        "Location": " London",
+        "Location": "Nahda",
         "price": 150,
         "image":"images/karate.png",
         "availableInventory": 10,
@@ -32,8 +29,7 @@ let products = [
     {
         "id": 1004,
         "subject": "Yoga",
-        "description": "fun math class",
-        "Location": " California",
+        "Location": "Jumeirah",
         "price": 140,
         "image":"images/yoga.png",
         "availableInventory": 10,
@@ -42,8 +38,7 @@ let products = [
     {
         "id": 1005,
         "subject": "Table Tennis",
-        "description": "fun math class",
-        "Location": " Alaska",
+        "Location": "Qusais",
         "price": 120,
         "image":"images/tabletennis.png",
         "availableInventory": 10,
@@ -52,8 +47,7 @@ let products = [
     {
         "id": 1006,
         "subject": "Art",
-        "description": "fun math class",
-        "Location": " Columbia",
+        "Location": "Barsha",
         "price": 90,
         "image":"images/art.png",
         "availableInventory": 10,
@@ -62,8 +56,7 @@ let products = [
     {
         "id": 1007,
         "subject": "Music",
-        "description": "fun math class",
-        "Location": " Melbourne",
+        "Location": "Al Warqa",
         "price": 100,
         "image":"images/music.png",
         "availableInventory": 10,
@@ -72,8 +65,7 @@ let products = [
     {
         "id": 1008,
         "subject": "Dance",
-        "description": "fun math class",
-        "Location": " Columbia",
+        "Location": "Nahda",
         "price": 90,
         "image":"images/dance.png",
         "availableInventory": 10,
@@ -82,8 +74,7 @@ let products = [
     {
         "id": 1009,
         "subject": "Coding",
-        "description": "fun math class",
-        "Location": " Dubai",
+        "Location": "Internet City",
         "price": 130,
         "image":"images/coding.png",
         "availableInventory": 10,
@@ -92,8 +83,7 @@ let products = [
     {
         "id": 1010,
         "subject": "Football",
-        "description": "fun math class",
-        "Location": " London",
+        "Location": "Academic City",
         "price": 100,
         "image":"images/football.png",
         "availableInventory": 10,
